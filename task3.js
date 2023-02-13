faaaf0a34aa486bd746f099828215737c796eb2c
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-13 19:34:07 +0000

    task3 completed

---
+++ task3.js
@@ -0,0 +1,4 @@
+var n = "ibrahim ghani";
+console.log("Lowercase: ".concat(n.toLowerCase()));
+console.log("Uppercase: ".concat(n.toUpperCase()));
+console.log("Titlecase: ".concat(n.charAt(0).toUpperCase() + n.slice(1)));
---
+++ task3.ts
@@ -0,0 +1,5 @@
+let n: string = "ibrahim ghani";
+
+console.log(`Lowercase: ${n.toLowerCase()}`);
+console.log(`Uppercase: ${n.toUpperCase()}`);
+console.log(`Titlecase: ${n.charAt(0).toUpperCase() + n.slice(1)}`);
