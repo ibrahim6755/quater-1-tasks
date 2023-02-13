6b48c767d0b1f108c5c12b8774db93df7509bb99
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-13 19:55:10 +0000

    task6 completed

---
+++ task6.js
@@ -0,0 +1,4 @@
+var person = "\t \n \tJorge don\t \n ";
+console.log("Original name: \"".concat(person, "\""));
+var strippedPerson = person.trim();
+console.log("Stripped name: \"".concat(strippedPerson, "\""));
---
+++ task6.ts
@@ -0,0 +1,5 @@
+let person: string = "\t \n \tJorge don\t \n ";
+console.log(`Original name: "${person}"`);
+
+let strippedPerson = person.trim();
+console.log(`Stripped name: "${strippedPerson}"`);
