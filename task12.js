3c730e1359bba0c251cd432b676f53581f983bb1
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-13 20:31:36 +0000

    task12 completed

---
+++ task12.js
@@ -0,0 +1,4 @@
+var names = ["Ali", "Haider", "Aryan", "Hadi", "Haroon"];
+names.map(function (name) {
+    console.log("Hello, ".concat(name, "! How are you ?"));
+});
---
+++ task12.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Ali","Haider","Aryan","Hadi","Haroon"];
+
+names.map(name =>{
+    console.log(`Hello, ${name}! How are you ?`);
+})
