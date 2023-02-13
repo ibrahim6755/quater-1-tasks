f80780a30126eb3cf78e111d29680374d768eccd
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-13 20:40:44 +0000

    task14 completed

---
+++ task14.js
@@ -0,0 +1,4 @@
+var guests = ["Haroon", "Feroz", "faisal"];
+guests.map(function (guest) {
+    console.log("Dear ".concat(guest, " I'm sending you my warmest invitation to this dinner party"));
+});
---
+++ task14.ts
@@ -0,0 +1,5 @@
+let guests: string[]= ["Haroon","Feroz","faisal"];
+guests.map(guest=>{
+    console.log(`Dear ${guest} I'm sending you my warmest invitation to this dinner party`)
+
+});
