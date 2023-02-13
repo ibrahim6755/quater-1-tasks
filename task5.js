1e04b45fae4998dc8d72956619807ce5a5facae9
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-13 19:48:59 +0000

    task5 completed

---
+++ task5.js
@@ -0,0 +1,3 @@
+var famous_person = "Quaid e Azam";
+var message = "".concat(famous_person, " once said, \u201CI do not believe in taking the right decision, I take a decision and make it right.\u201D");
+console.log(message);
---
+++ task5.ts
@@ -0,0 +1,3 @@
+let famous_person: string = "Quaid e Azam";
+let message: string = `${famous_person} once said, “I do not believe in taking the right decision, I take a decision and make it right.”`;
+console.log(message);
