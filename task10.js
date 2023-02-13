47b5d3b069949824e0a672a8b1635999e60a36b9
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-13 20:17:08 +0000

    task10 completed

---
+++ task10.js
@@ -0,0 +1,3 @@
+var a = "ibrahim ghani";
+console.log("Lowercase: ".concat(a.toLowerCase())); //It will convert the written satement into lowercase
+console.log("Uppercase: ".concat(a.toUpperCase())); //It will convert the written satement into uppercase
---
+++ task10.ts
@@ -0,0 +1,4 @@
+let a: string = "ibrahim ghani";
+
+console.log(`Lowercase: ${a.toLowerCase()}`);//It will convert the written satement into lowercase
+console.log(`Uppercase: ${a.toUpperCase()}`);//It will convert the written satement into uppercase
