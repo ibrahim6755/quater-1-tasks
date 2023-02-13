3c9a2195b478b42232db08cce1acab3256287229
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-13 19:37:44 +0000

    task4 completed

---
+++ task4.js
@@ -0,0 +1,3 @@
+var author = 'Quaid e Azam once said,';
+var qoute = '“I do not believe in taking the right decision, I take a decision and make it right.”';
+console.log(author + qoute);
---
+++ task4.ts
@@ -0,0 +1,3 @@
+let author = 'Quaid e Azam once said,';
+let qoute = '“I do not believe in taking the right decision, I take a decision and make it right.”';
+console.log(author+qoute)
