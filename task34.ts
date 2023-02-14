43a383db5744b685447cf9f12ee0852194569f0b
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-14 20:18:05 +0000

    task34 completed

---
+++ task34.ts
@@ -0,0 +1,7 @@
+const favoritePizzas: string[] = ['pepperoni', 'margherita', 'hawaiian'];
+
+for (let pizza of favoritePizzas) {
+  console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("I really love pizza!");
