2cb2a9a45d117b958c2e3a94fcf5f73c68a59d53
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-14 19:25:53 +0000

    task27 completed

---
+++ task27.js
@@ -0,0 +1,33 @@
+// Version for green alien
+var alien_color = 'green';
+if (alien_color == 'green') {
+    console.log("The player earned 5 points.");
+}
+else if (alien_color == 'yellow') {
+    console.log("The player earned 10 points.");
+}
+else if (alien_color == 'red') {
+    console.log("The player earned 15 points.");
+}
+// Version for yellow alien
+alien_color = 'yellow';
+if (alien_color == 'green') {
+    console.log("The player earned 5 points.");
+}
+else if (alien_color == 'yellow') {
+    console.log("The player earned 10 points.");
+}
+else if (alien_color == 'red') {
+    console.log("The player earned 15 points.");
+}
+// Version for red alien
+alien_color = 'red';
+if (alien_color == 'green') {
+    console.log("The player earned 5 points.");
+}
+else if (alien_color == 'yellow') {
+    console.log("The player earned 10 points.");
+}
+else if (alien_color == 'red') {
+    console.log("The player earned 15 points.");
+}
