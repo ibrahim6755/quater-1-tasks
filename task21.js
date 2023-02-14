6335efa21874d3e8f398a65ccb6a917e3859f360
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-14 10:45:55 +0000

    task21 completed

---
+++ task21.js
@@ -0,0 +1,21 @@
+var usa = {
+    name: 'United States',
+    capital: 'Washington D.C.',
+    population: 33106,
+    language: 'English'
+};
+var canada = {
+    name: 'Canada',
+    capital: 'Ottawa',
+    population: 38984,
+    language: 'English, French'
+};
+var japan = {
+    name: 'Japan',
+    capital: 'Tokyo',
+    population: 12603,
+    language: 'Japanese'
+};
+console.log(usa);
+console.log(canada);
+console.log(japan);
---
+++ task21.ts
@@ -0,0 +1,25 @@
+const usa = {
+    name: 'United States',
+    capital: 'Washington D.C.',
+    population: 33106,
+    language: 'English'
+  };
+  
+  const canada = {
+    name: 'Canada',
+    capital: 'Ottawa',
+    population: 38984,
+    language: 'English, French'
+  };
+  
+  const japan = {
+    name: 'Japan',
+    capital: 'Tokyo',
+    population: 12603,
+    language: 'Japanese'
+  };
+  
+  console.log(usa);
+  console.log(canada);
+  console.log(japan);
+  
