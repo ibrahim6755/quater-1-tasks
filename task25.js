6366d914cb533f84948e354c3b49dc8694438143
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-14 19:12:08 +0000

    task25 completed

---
+++ task25.js
@@ -0,0 +1,10 @@
+// Version that passes the if test
+var alien_color = 'green';
+if (alien_color == 'green') {
+    console.log("The player just earned 5 points.");
+}
+// Version that fails the if test
+var alienColor = 'yellow';
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+}
---
+++ task25.ts
@@ -0,0 +1,14 @@
+// Version that passes the if test
+let alien_color = 'green';
+
+if (alien_color == 'green') {
+  console.log("The player just earned 5 points.");
+}
+
+// Version that fails the if test
+let alienColor = 'yellow';
+
+if (alienColor == 'green') {
+  console.log("The player just earned 5 points.");
+}
+
