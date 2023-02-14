2d22e38b3b235ac6295a8c8a80ef4c4c1b310667
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-14 19:19:01 +0000

    task26 completed

---
+++ task26.js
@@ -0,0 +1,16 @@
+// Version that runs the if block
+var alien_color = 'green';
+if (alien_color == 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points for shooting the alien.");
+}
+// Version that runs the else block
+var alienColor = 'yellow';
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points for shooting the alien.");
+}
---
+++ task26.ts
@@ -0,0 +1,18 @@
+// Version that runs the if block
+let alien_color = 'green';
+
+if (alien_color == 'green') {
+  console.log("The player just earned 5 points for shooting the alien.");
+} else {
+  console.log("The player just earned 10 points for shooting the alien.");
+}
+
+// Version that runs the else block
+let alienColor = 'yellow';
+
+if (alienColor == 'green') {
+  console.log("The player just earned 5 points for shooting the alien.");
+} else {
+  console.log("The player just earned 10 points for shooting the alien.");
+}
+
