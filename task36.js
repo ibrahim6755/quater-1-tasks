b9801f87e76a99d4b532a23402f5ed2475dd22aa
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-14 20:32:09 +0000

    task36 completed

---
+++ task36.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("The shirt size is ".concat(size, " and it will have the message \"").concat(message, "\" printed on it."));
+}
+make_shirt("M", "Hello, World!"); // Output: The shirt size is M and it will have the message "Hello, World!" printed on it.
---
+++ task36.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string, message: string) {
+    console.log(`The shirt size is ${size} and it will have the message "${message}" printed on it.`);
+  }
+  
+  make_shirt("M", "Hello, World!"); // Output: The shirt size is M and it will have the message "Hello, World!" printed on it.
+  
