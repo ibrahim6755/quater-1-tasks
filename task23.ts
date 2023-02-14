5116a65707553921c5de164a147ff61a54aca4d3
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-14 18:46:41 +0000

    task23 completed

---
+++ task23.ts
@@ -0,0 +1,36 @@
+let x = 5;
+let y = 10;
+let personName = 'Ali';
+let age = 30;
+let isStudent = true;
+
+
+console.log("Is name equal to 'Ali'? I predict True.");
+console.log(personName == 'Ali');
+
+console.log("Is age greater than or equal to 18? I predict True.");
+console.log(age >= 18);
+
+console.log("Is x less than or equal to 5 AND y greater than 5? I predict True.");
+console.log(x <= 5 && y > 5);
+
+console.log("Is x greater than 3 AND y less than 12? I predict True.");
+console.log(x > 3 && y < 12);
+
+console.log("Is isStudent not equal to false? I predict True.");
+console.log(isStudent =! false);
+
+console.log("Is x greater than y? I predict False.");
+console.log(x > y);
+
+console.log("Is x equal to y? I predict False.");
+console.log(x == y);
+
+console.log("Is name not equal to 'Ali' OR age less than 25? I predict False.");
+console.log(personName != 'Ali' || age < 25);
+
+console.log("Is age greater than 50? I predict False.");
+console.log(age > 50);
+
+console.log("Is isStudent equal to false? I predict False.");
+console.log(isStudent = false);
