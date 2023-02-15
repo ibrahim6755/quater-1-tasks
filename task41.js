072bec9692c4f09c2dec92068bfb8b3d5fc3b9a1
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-15 09:59:16 +0000

    task41 completed

---
+++ task41.js
@@ -0,0 +1,8 @@
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magician);
+    }
+}
+var magician_names = ["David Copperfield", "Penn Jillette", "Teller", "David Blaine"];
+show_magicians(magician_names);
---
+++ task41.ts
@@ -0,0 +1,7 @@
+function show_magicians(magicians:string[]){
+    for(const magician of magicians){
+        console.log(magician);
+    }
+}
+const magician_names: string[] =["David Copperfield", "Penn Jillette", "Teller", "David Blaine"];
+show_magicians(magician_names);
