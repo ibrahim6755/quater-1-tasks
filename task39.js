80a368928dcffb68789d05018e6e27fa123fbdd3
ibrahim6755 <ibrahimghani020@gmail.com>
2023-02-15 09:35:05 +0000

    task39 completed

---
+++ task39.js
@@ -0,0 +1,9 @@
+function city_country(city, country) {
+    console.log("".concat(city, ",").concat(country));
+}
+var result_1 = city_country("Lahore", "Pakistan");
+console.log(result_1);
+var result_2 = city_country("Newyork", "United States");
+console.log(result_2);
+var result_3 = city_country("Sydney", "Australia");
+console.log(result_3);
---
+++ task39.ts
@@ -0,0 +1,11 @@
+function  city_country(city:string, country:string){
+    console.log(`${city},${country}`);
+}
+const result_1 = city_country("Lahore","Pakistan");
+console.log(result_1);
+
+const result_2 = city_country("Newyork","United States");
+console.log(result_2);
+
+const result_3 = city_country("Sydney","Australia");
+console.log(result_3);
